aaffcf238b32ed2db6bb8ce5ab66ba5bfb0b7436
Simone Formica <simone.formica@nttdata.com>
2026-07-08 14:17:48 +0200

    Feat(timesheet): Upgrade version name

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -75,8 +75,8 @@ android {
         applicationId = "com.tlincompose"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.12"
     }
 
     compileOptions {
